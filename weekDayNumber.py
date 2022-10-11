6a3d9caa7cb6ec78690f25655d6763280a71619c
Shivam Kaushal <shivamkaushal907@gmail.com>
2022-10-11 09:42:34 +0530

    Create weekDayNumber.py

---
+++ weekDayNumber.py
@@ -0,0 +1,32 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Hello world")
+d = input("Enter week day name or number: ")
+if(d=="monday"):
+    print("Week day number 1")
+elif(d=="tuesday"):
+    print("Week day number 2")
+elif(d=="wednesday"):
+    print("Week day number 3")
+elif(d=="thursday"):
+    print("Week day number 4")
+elif(d=="friday"):
+    print("Week day number 5")
+elif(d=="saturday"):
+    print("Week day number 6")
+elif(d=="sunday"):
+    print("Week day number 7")
+elif(d=="1"):
+    print("Week day is Monday")
+elif(d=="2"):
+    print("Week day is Tuesday")
+elif(d=="3"):
+    print("Week day is Wednesday")
+elif(d=="4"):
+    print("Week day is Thursday")
+elif(d=="5"):
+    print("Week day is Friday")
+elif(d=="6"):
+    print("Week day is Saturday")
+elif(d=="7"):
+    print("Week day is Sunday")
